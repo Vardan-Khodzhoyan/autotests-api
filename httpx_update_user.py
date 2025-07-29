ee8777878782f57cf6993478925e07aadca2a65b
Vardan-Khodzhoyan <khodzhoyan.v@gmail.com>
2025-07-29 12:33:12 +0300

    Знакомство с API клиентами

--- httpx_update_user.py
+++ httpx_update_user.py
@@ -23,13 +23,20 @@ login_response = httpx.post("http://localhost:8000/api/v1/authentication/login",
 login_response_data = login_response.json()
 print('Login data:', login_response_data)
 
-# Удаляем ранее созданного пользователя
-delete_user_headers = {
+# Обновление ранее созданного пользователя
+update_user_headers = {
     "Authorization": f"Bearer {login_response_data['token']['accessToken']}"
 }
-delete_user_response = httpx.delete(
+update_user_payload = {
+    "email": get_random_email(),
+    "lastName": "string1",
+    "firstName": "string1",
+    "middleName": "string1"
+}
+update_user_response = httpx.patch(
     f"http://localhost:8000/api/v1/users/{create_user_response_data['user']['id']}",
-    headers=delete_user_headers
+    json=update_user_payload,
+    headers=update_user_headers
 )
-delete_user_response_data = delete_user_response.json()
-print('Delete user data:', delete_user_response_data)
+update_user_response_data = update_user_response.json()
+print('Update user data:', update_user_response_data)
